56fd23bf533416c9954b7ebb887b5f716c82ad5e
AlexeyMokrinskiy <ambelimo@yandex.ru>
2023-07-07 00:08:45 +0300

    Task_08 remove

--- Task_08.sql
+++ Task_08.sql
@@ -13,14 +13,12 @@ CREATE TABLE animals(
 
 CREATE TABLE pet_animals_type(
 	Animal_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    Animal_kind VARCHAR(45),
-    FOREIGN KEY (Animal_id) REFERENCES animals(Animal_id)
+    Animal_kind VARCHAR(45)
 );
 
 CREATE TABLE pack_animals_type(
 	Animal_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    Animal_kind VARCHAR(45),
-    FOREIGN KEY (Animal_id) REFERENCES animals(Animal_id)
+    Animal_kind VARCHAR(45)
 );
 
 -- Создаём таблицы с самими животными: 
@@ -29,48 +27,42 @@ CREATE TABLE dogs(
 	Animal_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     Animal_name VARCHAR(45),
     Animal_command VARCHAR(45),
-    Animal_birthday DATE,
-    FOREIGN KEY (Animal_id) REFERENCES pet_animals_type(Animal_id)
+    Animal_birthday DATE
 );
 
 CREATE TABLE cats(
 	Animal_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     Animal_name VARCHAR(45),
     Animal_command VARCHAR(45),
-    Animal_birthday DATE,
-    FOREIGN KEY (Animal_id) REFERENCES pet_animals_type(Animal_id)
+    Animal_birthday DATE
 );
 
 CREATE TABLE hamsters(
 	Animal_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     Animal_name VARCHAR(45),
     Animal_command VARCHAR(45),
-    Animal_birthday DATE,
-    FOREIGN KEY (Animal_id) REFERENCES pet_animals_type(Animal_id)
+    Animal_birthday DATE
 );
 
 CREATE TABLE horses(
 	Animal_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     Animal_name VARCHAR(45),
     Animal_command VARCHAR(45),
-    Animal_birthday DATE,
-    FOREIGN KEY (Animal_id) REFERENCES pack_animals_type(Animal_id)
+    Animal_birthday DATE
 );
 
 CREATE TABLE camels(
 	Animal_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     Animal_name VARCHAR(45),
     Animal_command VARCHAR(45),
-    Animal_birthday DATE,
-    FOREIGN KEY (Animal_id) REFERENCES pack_animals_type(Animal_id)
+    Animal_birthday DATE
 );
 
 CREATE TABLE donkeys(
 	Animal_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     Animal_name VARCHAR(45),
     Animal_command VARCHAR(45),
-    Animal_birthday DATE,
-    FOREIGN KEY (Animal_id) REFERENCES pack_animals_type(Animal_id)
+    Animal_birthday DATE
 );
 
 
