c0533dab582a18c2c74eaa8480aea9c67378c7b3
AlexeyMokrinskiy <ambelimo@yandex.ru>
2023-07-08 18:00:07 +0300

    Task_10 iproved

--- Task_10.sql
+++ Task_10.sql
@@ -5,11 +5,12 @@ USE humans_friends;
 
 TRUNCATE camels;
 
-INSERT INTO horses (Animal_name, Animal_command, Animal_birthday)
-SELECT Animal_name, Animal_command, Animal_birthday
+INSERT INTO horses (Animal_name, Animal_command, Animal_birthday, Kind_id)
+SELECT Animal_name, Animal_command, Animal_birthday, Kind_id
 FROM donkeys;
 
 DROP TABLE donkeys;
 
+RENAME TABLE horses TO horses_and_donkeys;
 
 
